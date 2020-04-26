0ff9c6d913d963dd0038bfbe52baf8c579b5962d
kendot <doherty.kenneth@gmail.com>
2020-04-26 15:26:21 -0400

    show date and message

--- checkfb.py
+++ checkfb.py
@@ -5,6 +5,7 @@ import notifiers
 import requests
 from pprint import pprint as pp
 from selenium import webdriver
+from bs4 import BeautifulSoup
 
 from twilio.rest import Client
 
@@ -13,15 +14,15 @@ FB = 'https://www.facebook.com/pg/'
 
 shelters = [
     'giffordcatshelter',
-    'northeastanimalshelter',
-    'brokentailrescue',
-    'buddydoghs',
-    'MelroseHumaneSociety',
-    'thesterlinganimalshelter',
-    'tccwaltham',
-    'NAShelter',
-    'tenlivescatrescue',
-    'blackcatrescue',
+    # 'northeastanimalshelter',
+    # 'brokentailrescue',
+    # 'buddydoghs',
+    # 'MelroseHumaneSociety',
+    # 'thesterlinganimalshelter',
+    # 'tccwaltham',
+    # 'NAShelter',
+    # 'tenlivescatrescue',
+    # 'blackcatrescue',
 ]
 
 options = webdriver.ChromeOptions()
@@ -30,17 +31,32 @@ options.add_argument('--incognito')
 options.add_argument('--headless')
 driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=options)
 
-print(dir(driver))
-
 for shelter in shelters:
     url = FB + shelter + '/posts'
     print('='*20)
     print(url)
     driver.get(url)
-    for post in driver.find_elements_by_class_name('userContent'):
-        print('-'*10)
-        # print(post.find_element('timestampContent'))
-        print(post.text)
+    page_source = driver.page_source
+
+    soup = BeautifulSoup(page_source, 'lxml')
+    posts = []
+
+    post_divs = soup.find_all('div', class_='userContentWrapper')
+
+    for post in post_divs:
+        # post_div = post.find('div', class_='userContent').text
+        # date_div = post.find('span', class_='timestampContent')
+        # post_div = post.find('div', class_='text_exposed_root')
+        date_text = post.find('span', class_='timestampContent').get_text()
+        date_text = post.find('span', class_='timestampContent').get_text()
+        post_text = post.find('div', class_='userContent').get_text()
+
+        print('{}: {}'.format(date_text, post_text))
+
 
 driver.quit()
+# for post in driver.find_elements_by_class_name('userContent'):
+#     print('-'*10)
+#     # print(post.find_element('timestampContent'))
+#     print(post.text)
 
